44cff351e85ef06a308fa65ae388cecb80491604
Oleh Khomei <varyform@gmail.com>
2024-03-03 11:20:32 +0200

    Cleanup, use array for BG, hash is broken in 5.18

--- app/main.rb
+++ app/main.rb
@@ -11,7 +11,7 @@ WHITE = { r: 255, g: 255, b: 255 }.freeze
 def tick(args)
   init(args) if args.state.tick_count.zero?
 
-  args.outputs.background_color = BLACK
+  args.outputs.background_color = [0, 0, 0]
 
   cells = args.state.world.cells
 
@@ -28,10 +28,6 @@ def tick(args)
   }.merge(WHITE)
 
   args.state.world.next_generation! # if args.state.tick_count.mod_zero?(20)
-end
-
-def bg(args, color: BLACK)
-  args.outputs.solids << { x: args.grid.left, y: args.grid.bottom, w: args.grid.w, h: args.grid.h, **color }
 end
 
 def init(args)
